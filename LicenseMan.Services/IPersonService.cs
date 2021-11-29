a8a455e9746e5630fcd837e1404cc270a60180b4
Arjo Kalisvaart <amplifiedart@gmail.com>
2021-11-29 22:23:05 +0100

    Added Person Service

---
+++ LicenseMan.Services/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using LicenseMan.GeneralEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseMan.Services
+{
+	public interface IPersonService
+	{
+		Task CreateAsync(Person newPerson);
+		Person GetById(int id);
+		Task UpdateAsync(Person person);
+		Task UpdateAsync(in id);
+		Task Delete(int id);
+		IEnumerable<Person> GetAll();
+	}
+}
---
+++ LicenseMan.Services/Service/PersonService.cs
@@ -0,0 +1,56 @@
+﻿using LicenseMan.ApplicationPersistence;
+using LicenseMan.GeneralEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseMan.Services.Service
+{
+	public class PersonService : IPersonService
+	{
+		private readonly ApplicationDbContext context;
+
+		public PersonService(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+
+		public async Task CreateAsync(Person newPerson)
+		{
+			await context.Persons.AddAsync(newPerson);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task Delete(int id)
+		{
+			var person = GetById(id);
+			context.Remove(person);
+			await context.SaveChangesAsync();
+		}
+
+		public IEnumerable<Person> GetAll()
+		{
+			return context.Persons;
+		}
+
+		public Person GetById(int id)
+		{
+			return context.Persons.Where(p => p.Id == id).FirstOrDefault();
+		}
+
+		public async Task UpdateAsync(Person person)
+		{
+			context.Update(person);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task UpdateAsync(int id )
+		{
+			var person = GetById(id);
+			context.Update(person);
+			await context.SaveChangesAsync();
+		}
+	}
+}
